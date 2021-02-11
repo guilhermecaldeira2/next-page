b1b54b36db7d0f64631502442e982e9064d92b96
Guilherme Caldeira <guilhermecaldeira.godoy@gmail.com>
2021-02-10 22:25:18 -0300

    remove server side css injection

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head'
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -6,6 +7,14 @@ import Navigation from "../components/Navigation";
 
 function MyApp({ Component, pageProps }) {
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
 
   const theme = React.useMemo(
     () =>
@@ -18,11 +27,16 @@ function MyApp({ Component, pageProps }) {
   );
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-      <Navigation />
-    </ThemeProvider>
+    <>
+    <Head>
+      <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+    </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+        <Navigation />
+      </ThemeProvider>
+    </>
   );
 }
 
